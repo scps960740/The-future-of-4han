a24a32220ffe0ee5af940930ce469c9a418580bc
Yixun Tseng <yixun.tseng@binance.com>
2020-11-17 00:01:49 +0800

    first commit

---
+++ 01/variable.js
@@ -0,0 +1,51 @@
+
+
+// console.log(1) // 數字 number (整數，小數)
+// console.log("stringgggg") // 字串 string = 內容
+// console.log(true) // 布林值 boolean true (1) or false (0)
+// console.log(null) // null 什麼都沒有
+// console.log(undefined) // undefined 什麼都沒有定義
+
+// JAVA 寫法  JAVA => 強類型語言
+// Int num = 100 
+// String num = "我是字串"
+// Float num = 1.1111
+// Boolean boo = true or false
+// Object n = null
+
+// JS 寫法  JS => 弱類型語言
+// var let const
+let num = 100
+let float = 100.111
+let name = "許善"
+let boo = true
+let boo2 = false
+let n = null
+let und = undefined
+
+// Assignment to constant variable.
+// 分配 Assignment
+// constant 常數
+// variable 變數
+const num1 = 1000
+
+// console.log(num)
+// console.log(name)
+
+// num = 99 // 換掉100 變-> 99
+// name = "育綸"
+
+// error 錯誤
+// num1 = 999
+
+// console.log(num)
+// console.log(num1)
+// console.log(name)
+
+console.log(boo)
+console.log(boo2)
+console.log(n)
+console.log(und)
+
+// 不相等 null !== undefinec
+console.log(und === n);
