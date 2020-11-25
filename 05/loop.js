33f91c74548ab3b7dd26d9fa7bdcbb7fc062453c
Yixun Tseng <yixun.tseng@binance.com>
2020-11-26 00:16:42 +0800

    for

---
+++ 05/loop.js
@@ -0,0 +1,39 @@
+// for
+
+
+// 計數器
+
+// for (let i = 0; i < 5; i++) {
+//   console.log(i);
+// }
+
+// for (let i = 5; i >= 0; i--) {
+//   console.log(i);
+// }
+
+// let i = 5
+// for (; i >= 0; i--) {
+//   console.log(i);
+// }
+// // TODO del
+// console.log('外面', i);
+
+
+// let sum = 0
+// for (let i = 5; i >= 0; i--) {
+//   sum = sum + i
+// }
+
+
+for (let i = 0; i < 9; i++) {
+
+  console.log('i-----', i);
+
+  for (let k = 0; k < 9; k++) {
+    console.log(k);
+  }
+
+}
+
+// 群組有三組
+// 每組有兩個人
