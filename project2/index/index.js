e8c378b1630008b2657b737711245a29d27a5421
Baozi960101 <Baozi960101@gmail.com>
2021-01-27 18:39:00 +0800

    0127_4Han

--- project2/index/index.js
+++ project2/index/index.js
@@ -1,9 +1,9 @@
 window.addEventListener('load', function () {
 
-    
-    
+
+
     const title = document.getElementById("title")
-    
+
     // 兩種用法 一種用querySelector抓class 一種用getElementById抓id
     const starbutton = document.querySelector(".starButton")
 
@@ -21,5 +21,20 @@ window.addEventListener('load', function () {
     stopwatch.addEventListener("click", function () {
         title.innerText = "Stopwatch"
         starbutton.href = "/project/Stopwatch/index.html"
+        })
     })
-})
+
+    const meanbar = document.getElementById('meanbar')
+
+    const sidebar = document.getElementById("sidebar")
+    sidebar.addEventListener("click", function () {
+        sidebar.style.right = "-9999px"
+        meanbar.style.right = "-9999px"
+    })
+    
+
+    const mobileheaderContent2 = document.getElementById('mobileheaderContent2')
+    mobileheaderContent2.addEventListener('click',function(){
+        meanbar.style.right = "0px"
+        sidebar.style.right = "0px"
+    })
