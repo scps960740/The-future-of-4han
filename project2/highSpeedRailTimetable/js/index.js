1e54ea0c759f7d6275835f36fbfa3c49d1296e14
Baozi960101 <Baozi960101@gmail.com>
2021-02-25 20:11:04 +0800

    0225_4Han

--- project2/highSpeedRailTimetable/js/index.js
+++ project2/highSpeedRailTimetable/js/index.js
@@ -19,6 +19,7 @@ window.addEventListener("load", function () {
   const newDate = document.getElementById("newDate")
   const time = document.getElementById("time")
   const newTime = document.getElementById("newTime")
+  const mobileProject = document.getElementById("mobileProject")
 
 
   mobileTitleImg.addEventListener("click", function () {
@@ -35,5 +36,37 @@ window.addEventListener("load", function () {
     endStation.innerText = "終點站:" + `${moibleEndStation.value}`
     date.innerText = "日期:" + `${newDate.value}`
     time.innerText = "發車時間:" + `${newTime.value}`
+    mobileProject.innerText = "車次代碼:" + `${moibleStartStation.value}`
   })
+
+  const HOST_API = "https://ptx.transportdata.tw/MOTC"
+  const ALL_STATION_API = `${HOST_API}/v2/Rail/THSR/Station?$format=JSON`
+
+  $.get(ALL_STATION_API, function (date) {
+    const filterData = date.map(function (item, index) {
+      const stationID = item.StationID
+      const stationName = item.StationName.Zh_tw
+      return {
+        id: stationID,
+        name: stationName,
+      }
+    })
+    filterData.map(function (item) {
+      const optionNode = document.createElement("option")
+      optionNode.value = item.id
+      optionNode.innerHTML = item.name
+
+      moibleEndStation.appendChild(optionNode)
+      
+      // 不return就好
+    });
+    filterData.forEach((item) => {
+      const optionNode = document.createElement("option")
+      optionNode.value = item.id
+      optionNode.innerHTML = item.name
+
+      moibleStartStation.appendChild(optionNode)
+    });
+  })
+
 });
